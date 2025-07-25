966edbfe935ab9487eed3cfc59ce6506db6a1b98
David Brainard <brainard@psych.upenn.edu>
2025-07-25 12:45:23 -0400

    Comments

--- Psychtoolbox/PsychColorimetricData/ComputeCIEConeFundamentals.m
+++ Psychtoolbox/PsychColorimetricData/ComputeCIEConeFundamentals.m
@@ -100,6 +100,10 @@ function [T_quantalAbsorptionsNormalized,T_quantalAbsorptions,T_quantalIsomeriza
 %                                     in line 216 of ComputeRawConeFundamentals if params.DORODS is true.
 %    adjIndDiffParams.absorbance - Photopigment absorbance as given in line 188 of ComputeRawConeFundamentals
 %    adjIndDiffParams.absorptance - Photopigment absorptance as given in line 230 of ComputeRawConeFundamentals
+%    adjIndDiffParams.photopigmentQuantalEfficiency - Photopigment quantal efficiency as passed to ComptuteRawConeFundamentals.
+%                                     We have this here in staticParams,
+%                                     but useful to have in adjIndDiffParams as
+%                                     well, sometimes.
 %
 % For both adjIndDiffParams.mac and adjIndDiffParams.lens, the wavelength
 % spacing is the same as in the S input variable of this function.
